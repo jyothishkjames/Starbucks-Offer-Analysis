adfe9228250693283c2d1a34fc95249d2f0ef0a7
jyothishkjames <jyothishlp@gmail.com>
2020-08-18 16:01:57 +0200

    feat: implementing clean_data function

--- data/process_data.py
+++ data/process_data.py
@@ -11,5 +11,39 @@ def read_data():
     transcript = pd.read_json('../data/dataset/transcript.json', orient='records', lines=True)
 
 
-def clean_data():
-    pass
+def clean_data(profile, portfolio, transcript, offer, amount):
+    # Converting None to NAN
+    transcript['offer id'] = transcript['value'].apply(offer)
+    transcript['amount'] = transcript['value'].apply(amount)
+
+    # Rename column 'id' to 'person'
+    profile = profile.rename(columns={'id': 'person'})
+
+    # Rename column 'id' to 'offer id'
+    portfolio = portfolio.rename(columns={'id': 'offer id'})
+
+    # Merge dataframes proflie and transcript
+    merged_df = profile.merge(transcript, how='right', on='person')
+
+    # Drop Nan values in column 'Gender', 'Income'
+    merged_df = merged_df.dropna(subset=['income'])
+
+    # Drop column 'value'
+    merged_df.drop(columns=['value'], inplace=True)
+
+    # Create offer dataframe - offer_df
+    offer_df = merged_df.dropna(subset=['offer id'])
+
+    # Drop column 'amount' from offer_df dataframe
+    offer_df.drop(columns=['amount'], inplace=True)
+
+    # Merge dataframes portfolio and offer_df, map columns 'offer id' to 'offer type'
+    offer_type_df = portfolio.merge(offer_df, how='right', on='offer id')
+
+    # Create amount dataframe - amount_df
+    amount_df = merged_df.dropna(subset=['amount'])
+
+    # Drop column 'offer id'
+    amount_df.drop(columns=['offer id'], inplace=True)
+
+    return offer_type_df, amount_df
