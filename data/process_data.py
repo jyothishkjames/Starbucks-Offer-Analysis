31951836279a2d175b79f5b671d0a3376c949308
jyothishkjames <jyothishlp@gmail.com>
2020-08-19 18:59:32 +0200

    doc: adding doc string for functions

--- data/process_data.py
+++ data/process_data.py
@@ -5,6 +5,13 @@ import json
 
 
 def read_data():
+    """
+    OUTPUT:
+    portfolio - portfolio dataframe
+    profile - profile dataframe
+    transcript - transcript dataframe
+
+    """
     # read in the json files
     portfolio = pd.read_json('../data/dataset/portfolio.json', orient='records', lines=True)
     profile = pd.read_json('../data/dataset/profile.json', orient='records', lines=True)
@@ -14,6 +21,19 @@ def read_data():
 
 
 def clean_data(profile, portfolio, transcript, offer, amount):
+    """
+    INPUT:
+    profile - (pandas dataframe) profile as defined at the top of the notebook
+    portfolio - (pandas dataframe) portfolio as defined at the top of the notebook
+    transcript - (pandas dataframe) transcript as defined at the top of the notebook
+
+    OUTPUT:
+    offer_type_df - merged dataframe containing columns offer id, offer type, age,
+                    became_member_on, gender, person, income
+    amount_df - merged dataframe containing columns event, amount, age,
+                became_member_on, gender, person, income
+
+    """
     # Converting None to NAN
     transcript['offer id'] = transcript['value'].apply(offer)
     transcript['amount'] = transcript['value'].apply(amount)
@@ -49,3 +69,27 @@ def clean_data(profile, portfolio, transcript, offer, amount):
     amount_df.drop(columns=['offer id'], inplace=True)
 
     return offer_type_df, amount_df
+
+
+def match(offer_type_df, amount_df):
+    """
+    INPUT:
+    offer_type_df - (pandas dataframe) offer_type_df returned by function clean_data
+    amount_df - (pandas dataframe) amount_df returned by function clean_data
+
+    OUTPUT:
+    df_match - (pandas dataframe) dataframe which contains demographic groups that will
+                make purchases even if they don't receive an offer
+
+    """
+
+    persons_completed_offer = list(offer_type_df['person'][offer_type_df['event'] == 'offer completed'].unique())
+
+    df_match = amount_df[amount_df['age'] == 144]
+
+    for person in persons_completed_offer:
+        df_match = pd.concat([df_match, amount_df[amount_df['person'].isin([person])]])
+
+    return df_match
+
+
